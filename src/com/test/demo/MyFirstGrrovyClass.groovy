f243b50d13ec2b4261e95d9eff49811c2577a527
sushmita <susmitha.rakapali@gmail.com>
2020-12-27 20:51:39 -0800

    this is my first update to repo

--- src/com/test/demo/MyFirstGrrovyClass.groovy
+++ src/com/test/demo/MyFirstGrrovyClass.groovy
@@ -5,6 +5,8 @@ class MyFirstGrrovyClass {
 	static void main(args) {
 		print("Hello world\n");
 		
-		
+		print("this is the 1st statment \n");
+		print("this is the 2nd statment \n");
+		print("this is the 3rd statment \n");
 	}
 }
