c58fcdf3de25bfddb5a307013f0ea975698e4ecd
sushmita <susmitha.rakapali@gmail.com>
2020-12-27 22:23:06 -0800

    testttt

--- src/com/test/demo/MyFirstGrrovyClass.groovy
+++ src/com/test/demo/MyFirstGrrovyClass.groovy
@@ -8,5 +8,10 @@ class MyFirstGrrovyClass {
 		print("this is the 1st statment \n");
 		print("this is the 2nd statment \n");
 		print("this is the 3rd statment \n");
+		print("Hello world\n");
+		
+		print("this is the 4st statment \n");
+		print("this is the 5nd statment \n");
+		print("this is the 6rd statment \n");
 	}
 }
